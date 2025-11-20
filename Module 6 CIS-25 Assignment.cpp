ae0298ba5c4a3edbeefb5c166be21afd57bcd20f
Alv-in1 <alvinadandonon53@gmail.com>
2025-11-20 13:02:12 -0800

    Add files via upload

---
+++ Module 6 CIS-25 Assignment.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+//Creating a class Item
+class Item{
+    public:
+    string name;
+    int quantity;
+    
+    //A method saveToFile to write the data in the format: name, quantity
+    void saveToFile(){
+        ofstream out("items.txt");
+        if (out.is_open()) {
+            out<<name <<","<< quantity<<endl;
+            out.close();
+            cout <<"Item save to file."<<endl;
+            
+        }else{
+            cout<<"Unable to open file for writing."<<endl;
+        }
+    }
+    
+    //A method loadFromFile() to read and display data from the file
+    void loadFromFile(){
+        ifstream in("items.txt");
+        if (in.is_open()){
+            string line;
+            while (getline(in, line)){
+                cout <<"File content: "<< line <<endl;
+            }
+            in.close();
+        }else{
+            cout <<"Unable to open file for reading." << endl;
+        }
+    }
+};
+
+int main() {
+    Item tool;
+    tool.name = "Screwdriver";
+    tool.quantity = 10;
+    
+    tool.saveToFile();
+    tool.loadFromFile();
+    
+    return 0;
+}
+
+        
+        
+        
+        
+        
+        
+        
+    
